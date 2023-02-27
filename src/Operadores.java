eaf1257a255f7cf35939948168bddb20cc91090a
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-02-27 14:46:45 +0100

    added operadores class

---
+++ src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    // aritméticos
+
+    int numero1 = 10;
+    int numero2 = 30;
+
+    int resultadoSuma = numero1 + numero2;
+
+    //Lógicos
+
+    if (numero1 > numero2) {
+        System.out.println("El numero " + numero1 + " es mas alto que " + numero2);
+    };
+
+    //incremento y decremento =
+
+    numero1++; //deberia incrementar valor de numero1 en 1. Entonces = 11;
+    numero2--; //deberia reducir valor de numero2 en 1. Entonces = 29;
+}
