797cd82e5344faac26f94895299935d535985f55
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-02-28 09:51:23 +0100

    added continue and break to while loop

--- src/While.java
+++ src/While.java
@@ -1,9 +1,19 @@
 public class While {
     public static void main(String[] args) {
         int nivelDeComida = 0;
-        while (nivelDeComida < 5) {
-            System.out.println("Que hambre, me como mi bocadillo " + (nivelDeComida + 1));
+        while (nivelDeComida < 10) {
             nivelDeComida++;
+            if (nivelDeComida == 5) {
+
+                System.out.println("Creo que le regalaré mi bocadillo " + nivelDeComida + " a mi perro.");
+                continue;
+            } else if (nivelDeComida == 9){
+                System.out.println("Ay no! ya no puedo mas! no podre terminar los bocadillos.");
+                break;
+            }
+            System.out.println("Que hambre, me como mi bocadillo " + (nivelDeComida));
+
+
         }
         System.out.println("Ahora si, a dormir la siesta");
     }
