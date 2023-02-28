138204f702dfb1b7ac4f768c07418c5daea61805
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-02-28 15:57:59 +0100

    try catch and finally

---
+++ src/com/example/TryCatchMain.java
@@ -0,0 +1,19 @@
+package com.example;
+
+public class TryCatchMain {
+    public static void main(String[] args) {
+
+        try {
+            int result = 5 / 0;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();}
+        finally {
+            System.out.println("Cierre de recursos");
+        }
+
+        System.out.println("fin");
+
+    }
+
+
+}
