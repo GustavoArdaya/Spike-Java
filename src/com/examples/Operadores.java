02edc1ac5c5f77a3ed0be485990a7fa588d7ae1d
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-01 09:20:08 +0100

    modified some excercises according to second part of course

--- src/com/examples/Operadores.java
+++ src/com/examples/Operadores.java
@@ -2,7 +2,29 @@ package com.examples;
 
 public class Operadores {
     // aritméticos
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 30;
 
+        int resultadoSuma = numero1 + numero2;
+        int resultadoResta = numero1 - numero2;
+        int resultadoMultiplicacion = numero1 * numero2;
+        int resultadoDivision = numero1 / numero2;
+
+        System.out.println("El resultado de la suma es: " + resultadoSuma);
+        System.out.println("El resultado de la resta es: " + resultadoResta);
+        System.out.println("El resultado de la multiplicación es: " + resultadoMultiplicacion);
+        System.out.println("El resultado de la division es: " + resultadoDivision);
+
+        if (numero2 > numero1) {
+            System.out.println("El numero " + numero2 + " es mas alto que " + numero1);
+        }
+
+        int aumentado = numero1++; //deberia incrementar valor de numero1 en 1. Entonces = 11;
+        int reducido = numero2 --; //deberia reducir valor de numero2 en 1. Entonces = 29;
+
+        System.out.println("despues de incrementar el primer numero: " + numero1 + ". Y despues de reducir el segundo numero: " + numero2);
+    }
     int numero1 = 10;
     int numero2 = 30;
 
@@ -11,13 +33,10 @@ public class Operadores {
     //Lógicos
 
     /*
-    if (numero1 > numero2) {
-        System.out.println("El numero " + numero1 + " es mas alto que " + numero2);
-    };
+
      */
 
     //incremento y decremento =
 
-    int aumentado = numero1++; //deberia incrementar valor de numero1 en 1. Entonces = 11;
-    int reducido = numero2 --; //deberia reducir valor de numero2 en 1. Entonces = 29;
+
 }
