08cb08af7926058841048dc6190492b239a3c86b
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-02 12:34:11 +0100

    replace method technique. This one was a little harder to follow

---
+++ src/com/refactoring/replacemethod/good/Product.java
@@ -0,0 +1,46 @@
+package com.refactoring.replacemethod.good;
+
+public class Product {
+
+    double price;
+    int quantity;
+    double discount;
+    double shipping;
+
+    // constructores
+
+    // metodos
+
+    // tostring
+
+    public double calculatePrice(){
+        return new ProductPriceCalculator(this).calculatePrice();
+    }
+
+    // getter y setter
+    public double getPrice() {
+        return price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public double getDiscount() {
+        return discount;
+    }
+    public double getShipping() {
+        return shipping;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+    public void setShipping(double shipping) {
+        this.shipping = shipping;
+    }
+
+}
