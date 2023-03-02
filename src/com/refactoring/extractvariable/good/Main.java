da0e64473ce1e305ab75000b254d5aa168fc0849
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-02 11:58:28 +0100

    extract variable technique

--- src/com/refactoring/extractvariable/good/Main.java
+++ src/com/refactoring/extractvariable/good/Main.java
@@ -3,10 +3,26 @@ package com.refactoring.extractvariable.good;
 public class Main {
 
     void printProductPrice(Order order){
-        Double totalPrice = order.getPrice() * order.getQuantity() -
-                order.getOffer() + order.getShipping() * 2;
 
-        System.out.println(totalPrice);
+        // Precio de productos
+        Double quantityPrice = order.getPrice() * order.getQuantity();
+
+        // Descuento
+        Double discountPrice = order.getPrice() * order.getOffer();
+
+        // Calculo de precio de productos con descuento incluido.
+
+        Double finalPrice = quantityPrice - discountPrice;
+
+        // precio de envio
+        Double shippingCost = 0.0;
+        if (finalPrice < 50) {
+            shippingCost = 2.99;
+        }
+
+        // Precio definitivo
+
+        System.out.println(finalPrice + shippingCost);
     }
 
 }
