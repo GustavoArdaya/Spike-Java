dba9147c8e8fc5fbcd37fd7f62cab6727c4468b7
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-02 11:40:13 +0100

    extractclass technique

---
+++ src/com/refactoring/extractclass/good/Person.java
@@ -0,0 +1,12 @@
+package com.refactoring.extractclass.good;
+
+public class Person {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    private Address address;
+
+}
