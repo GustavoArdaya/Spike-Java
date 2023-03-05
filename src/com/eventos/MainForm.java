7e6f9b63ea09d68d1f2736ad40de6ea06efb0f34
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-05 20:46:24 +0100

    eventlistener on button click

--- src/com/eventos/MainForm.java
+++ src/com/eventos/MainForm.java
@@ -1,6 +1,8 @@
 package com.eventos;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm {
     private JTextPane pricePane;
@@ -8,6 +10,15 @@ public class MainForm {
     private JTextPane totalPane;
     private JButton buttonCalculate;
     private JPanel taxPanel;
+
+    public MainForm() {
+        buttonCalculate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("boton");
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("SwingTax");
