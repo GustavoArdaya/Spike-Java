2b733bd0b04f0b8d5afadac5c38a37d2fd575c47
GustavoArdaya <gustavo.ardaya@gmail.com>
2023-03-05 16:21:46 +0100

    input with bufferInputStream (not quite understood yet...)

--- src/com/entradasalida/InputStreamExample.java
+++ src/com/entradasalida/InputStreamExample.java
@@ -1,41 +1,38 @@
 package com.entradasalida;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-
+import java.io.*;
 
 
 public class InputStreamExample {
     public static void main(String[] args) {
         try {
             InputStream fichero = new FileInputStream("C:/Users/gusta/Dropbox/Mi PC (LAPTOP-T710QH4E)/Desktop/Factoria-F5/Spike_Java/java_spike/src/com/entradasalida/ejemplo.txt");
+            BufferedInputStream ficheroBuffer = new BufferedInputStream(fichero);
             try {
 
                 //Con bytes: (mas lento, para files pequeños)
-                byte[] datos = fichero.readAllBytes();
-
-                for (byte dato : datos) {
-                    System.out.print((char)dato);
-                }
+//                byte[] datos = fichero.readAllBytes();
+//
+//                for (byte dato : datos) {
+//                    System.out.print((char)dato);
+//                }
 
                 // con entero y bucle while (para files mas grandes)
 
-                int datos2 = fichero.read();
-                while (datos2!= -1) {
-                    // -1 es EOF (end of file)
-                    System.out.print((char)datos2);
-                    datos2 = fichero.read();
-                }
+//                int datos2 = fichero.read();
+//                while (datos2!= -1) {
+//                    // -1 es EOF (end of file)
+//                    System.out.print((char)datos2);
+//                    datos2 = fichero.read();
+                //}
 
-                // por "trozos", bucle while y toString();
+                // por "trozos", bucle while y toString() con bufferedInputStream;
 
-                byte datos3[] = new byte[5]; //trozos de 5
-                int datos4 = fichero.read(datos3);
+                //byte datos3[] = new byte[5]; //trozos de 5
+                int datos4 = ficheroBuffer.read();
                 while (datos4!= -1) {
-                    System.out.print(datos3.toString());
-                    datos4 = fichero.read(datos3);
+                    System.out.print((char)datos4);
+                    datos4 = ficheroBuffer.read();
                 }
 
             } catch (IOException e) {
